d654c0b5a2ffc17d0698a6a403c39019d9fc6c1f
shunsaku.yokota <sadawo0126@gmail.com>
2020-03-09 18:45:45 +0000

    DRYな日本時間表示

--- config/application.rb
+++ config/application.rb
@@ -31,3 +31,13 @@ module Yepu
     config.generators.system_tests = nil
   end
 end
+
+module TimeFormatSandbox
+  class Application < Rails::Application
+    # ...
+    # タイムゾーンを日本時間に設定
+    config.time_zone = 'Asia/Tokyo'
+    # デフォルトのロケールを日本（ja）に設定
+    config.i18n.default_locale = :ja
+  end
+end
