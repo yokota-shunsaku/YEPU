eff7cc6fcea1d1437e9a4b080061862037bcdd7a
shunsaku.yokota <sadawo0126@gmail.com>
2020-03-09 08:15:26 +0000

    genre投稿

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:content)
+    params.require(:post).permit(:content, :genre)
   end
 
   def correct_user
