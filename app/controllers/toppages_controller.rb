bc5d1fbc55d67af549b9c1b7f27d967e56d92363
shunsaku.yokota <sadawo0126@gmail.com>
2020-03-10 08:22:33 +0000

    三種類の場合分け表示実装

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,23 +2,22 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
     
-    #binding.pry
-    if params[:genre]
-    #そのジャンルのPost一覧を取得するコード
-      @posts = Post.where(genre: params[:genre]).order(id: :desc).page(params[:page]).per(3)
-    else
-    #全てのPost一覧を取得するコード
-      @posts = current_user.feed_posts.order(id: :desc).page(params[:page]).per(3)
-    end
-    
-    
+      #binding.pry
+      
+      
+      if params[:genre] == 'eat' || params[:genre] == 'exercise' || params[:genre] == 'beauty'
+      #そのジャンルのPost一覧を取得するコード
+        @posts = Post.where(genre: params[:genre]).order(id: :desc).page(params[:page]).per(3)
+       
+      elsif params[:genre] == 'all'
+      #全てのPost一覧を取得するコード
+        @posts = Post.order(id: :desc).page(params[:page]).per(3)
+        
+      else
+      #自分とフォローユーザーのPost一覧を取得するコード
+        @posts = current_user.feed_posts.order(id: :desc).page(params[:page]).per(3)
+       
+      end
     end
   end
-  
-  
-  
-  
-  #def genres
-    #@posts = Post.where(genre_id: @genre_id)
-  #end
 end
